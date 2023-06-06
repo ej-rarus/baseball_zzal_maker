f8297609f1acf62d3fb05231e2d876774b678faa
JuHee_Lee <you@example.com>
2023-06-06 17:24:09 +0900

    commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+
 const lineWidth = document.getElementById("line-width");
 const lineWidthText = document.getElementById("line-width-text");
 const color = document.getElementById("color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
-const canvas = document.querySelector("#canvas");
-const underCanvas = document.querySelector("#under-canvas");
+const canvas = document.getElementById("canvas");
+const underCanvas = document.getElementById("under-canvas");
 const ctx = canvas.getContext("2d");
 const underCtx = underCanvas.getContext("2d");
 
@@ -19,6 +20,17 @@ const modeBtn = document.getElementById("mode-btn");
 const eraserBtn = document.getElementById("eraser-btn");
 const resetBtn = document.getElementById("reset-btn");
 const downloadBtn = document.getElementById("export-btn");
+const teamDB = document.getElementById("team_DB");
+const teamLG = document.getElementById("team_LG");
+const teamSL = document.getElementById("team_SL");
+const teamKH = document.getElementById("team_KH");
+const teamHE = document.getElementById("team_HE");
+const teamKT = document.getElementById("team_KT");
+const teamKW = document.getElementById("team_KW");
+const teamLT1 = document.getElementById("team_LT1");
+const teamLT2 = document.getElementById("team_LT2");
+const teamND = document.getElementById("team_ND");
+const teamSD = document.getElementById("team_SD");
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
@@ -120,14 +132,138 @@ function onEraserClick() {
     ctx.strokeStyle = "white";
 }
 
-function onTeamSubmit(event) {
+function onClickteamDB(event){
     event.preventDefault();
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    const teamName = document.getElementById("team-select");
-    let image = document.getElementById(teamName.value);
-    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
+    const img = new Image();
+    const teamName = document.getElementById("doosan");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }
 
+function onClickteamLG(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("lotte");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }   
+
+function onClickteamSL(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("samsung");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }      
+
+function onClickteamKH(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("kiwoom");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }      
+
+function onClickteamHE(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("hanwha");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    } 
+
+function onClickteamKT(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("kia");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }     
+
+function onClickteamKW(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("kt");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }       
+
+function onClickteamLT1(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("lg1");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }
+
+function onClickteamLT2(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("lg2");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }
+
+function onClickteamND(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("nc");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }
+
+ function onClickteamSD(event){
+    event.preventDefault();
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const img = new Image();
+    const teamName = document.getElementById("ssg");
+    img.src = teamName.src;
+    console.log(img.src);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    };
+    }
+    
 function onFileChange(event) {
     const file = event.target.files[0];
     const url = URL.createObjectURL(file);
@@ -147,11 +283,21 @@ downloadBtn.addEventListener("click", onDownloadBtnClick);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 
-selectedTeam.addEventListener("submit", onTeamSubmit);
-
 modeBtn.addEventListener("click", onModeClick);
 eraserBtn.addEventListener("click", onEraserClick);
 resetBtn.addEventListener("click", onResetClick);
 
+teamDB.addEventListener("click", onClickteamDB);
+teamLG.addEventListener("click", onClickteamLG);
+teamSL.addEventListener("click", onClickteamSL);
+teamKH.addEventListener("click", onClickteamKH);
+teamHE.addEventListener("click", onClickteamHE);
+teamKT.addEventListener("click", onClickteamKT);
+teamKW.addEventListener("click", onClickteamKW);
+teamLT1.addEventListener("click", onClickteamLT1);
+teamLT2.addEventListener("click", onClickteamLT2);
+teamND.addEventListener("click", onClickteamND);
+teamSD.addEventListener("click", onClickteamSD);
 
 fileInput.addEventListener("change", onFileChange);
+
